86d6d5cbe0b94e50e9bbcf865a74c07c32b2b527
Fabian Quist <fabian.quist@201221.local>
2023-05-23 11:19:58 +0200

    implementing grpc server api

--- start.sh
+++ start.sh
@@ -2,8 +2,11 @@
 
 set -e # script will exit if a cmd returns a non-zero status
 
+# for k3d deployment
+# export DB_SOURCE=postgresql://root:secret@database-service:5432/simple_bank?sslmode=disable
+
 echo "[run db migration]"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "[start the app]"
-exec $@
+exec "$@"
