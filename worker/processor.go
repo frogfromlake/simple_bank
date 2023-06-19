9e658494c503402f7230c11e73ae265e12da575d
Fabian Quist <fabian.quist@201221.local>
2023-06-19 12:43:48 +0200

    handled errors and print logs for asynq workers

--- worker/processor.go
+++ worker/processor.go
@@ -4,12 +4,13 @@ import (
 	"context"
 
 	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog/log"
 	db "github.com/techschool/simplebank/db/sqlc"
 )
 
 const (
 	QueueCritical = "critical"
-	QueueDefault = "default"
+	QueueDefault  = "default"
 )
 
 type TaskProcessor interface {
@@ -28,8 +29,13 @@ func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskPr
 		asynq.Config{
 			Queues: map[string]int{
 				QueueCritical: 10,
-				QueueDefault: 5,
+				QueueDefault:  5,
 			},
+			ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+				log.Error().Err(err).Str("type", task.Type()).
+					Bytes("payload", task.Payload()).Msg("process task failed")
+			}),
+			Logger: NewLogger(),
 		},
 	)
 
